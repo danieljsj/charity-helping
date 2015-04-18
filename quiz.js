8969a494e8b0ce0434ac729d0ba99b7e47fa9f87
Daniel Schulz-Jackson <daniel@Lena.local>
2015-04-17 22:29:27 -0500

    mini tweaks

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
-app.controller('quiz', function($scope, $http){
+app.controller('quiz', function($scope, $http){ // how do I know I'll actually get $http as my second param!?
 	// var currentUserName = 'Daniel';
 	$scope.orgScore = function(org){
+		if ( ! $scope.user ) return 0;
 		var score = 0;
 		for (var i = $scope.questions.length - 1; i >= 0; i--) {
 			var qnName = $scope.questions[i].name;
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-readme.md
+
+
+There are charities. they have scores in a variety of areas. 
+
+You take a quiz, and it matches you with the closest charity... AS you take it :-)
