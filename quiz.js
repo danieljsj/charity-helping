6c5d67dc24d100cc5e2cbfd269edac4a3c665534
Daniel Schulz-Jackson <daniel@Lena.local>
2015-04-17 22:09:51 -0500

    users are now data

--- quiz.js
+++ quiz.js
@@ -1,26 +1,25 @@
 app.controller('quiz', function($scope, $http){
+	// var currentUserName = 'Daniel';
 	$scope.questions = [
-		{ name: 'ecology', response:5, prompt:'How important is saving the planet from global warming?'},
-		{ name: 'poverty', response:5, prompt:'How important is it to prevent poverty?'},
-		{ name: 'justice', response:5, prompt:'How important is it to assure social equality, justice, and respect for all?'}
+		{ name: 'ecology', prompt:'How important is saving the planet from global warming?'},
+		{ name: 'poverty', prompt:'How important is it to prevent poverty?'},
+		{ name: 'justice', prompt:'How important is it to assure social equality, justice, and respect for all?'}
 	];
 	$scope.orgScore = function(org){
 		var score = 0;
 		for (var i = $scope.questions.length - 1; i >= 0; i--) {
-			for (var key in org.responses) {
-				if ( key == $scope.questions[i].name ){
-					score += 
-						$scope.questions[i].response 
-						* 
-						org.responses[key]
-					;
-				}
-			};
+			var qnName = $scope.questions[i].name;
+			score += org.responses[qnName] * $scope.user.responses[qnName];
 		};
 		org.score = score;
 		return score;
 	}
 	$http.get('organizations.json').success(function(response){
 		$scope.organizations = response;
+	});
+	$http.get('users.json').success(function(response){
+		$scope.users = response;
+		$scope.user = $scope.users[0];
+		// $scope.$apply();
 	});
 });
